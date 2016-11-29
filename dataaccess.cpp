e3ac54454f8f3e9e21199f2571c4ea6414262817
dadisigursveinn <dadi.sigursveinn@gmail.com>
2016-11-29 10:18:20 +0000

    Work on DataAccess

--- dataaccess.cpp
+++ dataaccess.cpp
@@ -12,41 +12,33 @@ DataAccess::DataAccess()
 }
 void DataAccess::writePerson(Person person)
 {
-    /*
+    // create fstream to data.csv
     ifstream file ("data.csv");
     if(file.is_open())
     {
+        // write data from param: person to the file
         file << person.getName() << "," << person.getAge() << "\n";
     }
-    */
 }
 
 vector<Person> DataAccess::readPersons()
 {
-    /*
+    // http://stackoverflow.com/questions/34218040/how-to-read-a-csv-file-data-into-an-array
     ifstream file ("data.csv");
-    vector<string> data;
-
+    string line;
+    vector<vector<string> > parsedCsv;
     if(file.is_open())
     {
-        string s;
-        if(!getline (infile, s )) break;
-
-        isstringstream ss( s );
-        vector <string> record;
-
-        while(ss)
+        while(getline(data,line))
         {
-            string s;
-            if (!getline(ss, s, ',')) break;
-            record.push_back( s );
+            stringstream lineStream(line);
+            string cell;
+            vector<string> parsedRow;
+            while(getline(lineStream,cell, ','))
+            {
+                parsedRow.push_back(cell);
+            }
+            parsedCsv.push_back(parsedRow);
         }
-
-        data.push_back( record );
     }
-    if (!infile.eof())
-    {
-        // TODO: error handeling
-    }
-    */
 }
